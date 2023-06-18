3d179b707694dfe92dee79b4a64573b8c974048f
kvdpxne <kvdpxne@gmail.com>
2023-06-18 21:14:29 +0200

    Added comment to method of creating new user.

--- covilo-core/src/main/java/me/kvdpxne/covilo/application/UserLifecycleUseCase.java
+++ covilo-core/src/main/java/me/kvdpxne/covilo/application/UserLifecycleUseCase.java
@@ -5,5 +5,12 @@ import me.kvdpxne.covilo.domain.model.User;
 
 public interface UserLifecycleUseCase {
 
+  /**
+   * Creates a new user from the given user data if the unique identifier
+   * given in the data does not exist in the database.
+   *
+   * @throws UserAlreadyExistsException If the unique user identifier already
+   *                                    exists in the database.
+   */
   User createUser(final User user) throws UserAlreadyExistsException;
 }
