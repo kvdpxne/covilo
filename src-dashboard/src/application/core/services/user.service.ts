a3410256d8dafa5318d572a6b627446a5df00994
kvdpxne <kvdpxne@gmail.com>
2024-03-27 11:13:48 +0100

    🔢 Implement pagination in UserService.

--- src-dashboard/src/application/core/services/user.service.ts
+++ src-dashboard/src/application/core/services/user.service.ts
@@ -1,45 +1,54 @@
 import {Injectable} from "@angular/core";
-import {ApiHttpClientService} from "../../shared";
 import {Observable} from "rxjs";
 import {User} from "../models/user";
-import {UserMeChangePasswordRequest} from "../playloads/user-me-change-password-request";
-import {UserMeChangeEmailRequest} from "../playloads/user-me-change-email-request";
+import {HttpBridge} from "../../shared/services/http/http-bridge";
+import {ApiHttpBridge} from "../../shared/services/http/api-http-bridge";
+import {HttpParams} from "@angular/common/http";
+import {Page} from "../aggregation/page";
+import {PageRequest} from "../aggregation/page-request";
+import {
+  HttpRequestOptions
+} from "../../shared/services/http/http-request-options";
 
 @Injectable({
   providedIn: "root"
 })
 export class UserService {
 
-  private readonly httpClientService: ApiHttpClientService;
+  private readonly httpBridge: HttpBridge;
 
-  public constructor(httpClientService: ApiHttpClientService) {
-    this.httpClientService = httpClientService;
+  public constructor(
+    apiHttpBridge: ApiHttpBridge
+  ) {
+    this.httpBridge = apiHttpBridge;
   }
 
   /**
+   * Retrieves a page of users from the backend.
    *
+   * If no page request is provided, the first page with a default page size
+   * of 20 is requested.
+   *
+   * @param pageRequest The page request is specifying the page number and
+   * page size.
+   * @returns An observable emitting the requested page of users.
    */
-  public get me(): Observable<User> {
-    return this.httpClientService.get<User>("me");
-  }
+  public getUsers(
+    pageRequest: PageRequest = {
+      pageNumber: 0,
+      pageSize: 20
+    }
+  ): Observable<Page<User>> {
+    return this.httpBridge.get<Page<User>>(
+      "api/v1/user/all",
+      (options: HttpRequestOptions): HttpRequestOptions => {
+        // Configure parameters for pagination
+        options.params = new HttpParams()
+          .append("page", pageRequest.pageNumber)
+          .append("size", pageRequest.pageSize);
 
-  public updateEmail(
-    request: UserMeChangeEmailRequest
-  ): Observable<void> {
-    return this.httpClientService.put<void>("me/email", request);
-  }
-
-  public updatePassword(
-    request: UserMeChangePasswordRequest
-  ): Observable<void> {
-    return this.httpClientService.put<void>("me/password", request);
-  }
-
-  public uploadAvatar(file: FormData): Observable<void> {
-    return this.httpClientService.post<void>("me/avatar", file);
-  }
-
-  public deleteAvatar(): Observable<void> {
-    return this.httpClientService.delete<void>("me/delete");
+        return options;
+      }
+    );
   }
 }
