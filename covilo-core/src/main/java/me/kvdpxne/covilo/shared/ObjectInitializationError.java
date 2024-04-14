82656d1d8a89e356c970dd799fc266ca162d872d
kvdpxne <kvdpxne@gmail.com>
2024-04-14 22:48:13 +0200

    Added javdoc for ObjectInitializationError.java.

--- covilo-core/src/main/java/me/kvdpxne/covilo/shared/ObjectInitializationError.java
+++ covilo-core/src/main/java/me/kvdpxne/covilo/shared/ObjectInitializationError.java
@@ -1,8 +1,16 @@
 package me.kvdpxne.covilo.shared;
 
+/**
+ * An error thrown to indicate that an attempt was made to initialize an object
+ * that should not be initialized.
+ */
 public final class ObjectInitializationError
   extends Error {
 
+  /**
+   * Constructs a new {@link ObjectInitializationError} with a default error
+   * message. The message indicates that the object should not be initialized.
+   */
   public ObjectInitializationError() {
     super("This object should not be initialized.");
   }
