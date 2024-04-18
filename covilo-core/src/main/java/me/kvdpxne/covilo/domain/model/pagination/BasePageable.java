781fcdf7dcc1d0772baf34dc42aaac6f2f9a5b2a
kvdpxne <kvdpxne@gmail.com>
2024-04-18 19:51:42 +0200

    📄 Implemented custom pagination classes for managing paginated collections.

---
+++ covilo-core/src/main/java/me/kvdpxne/covilo/domain/model/pagination/BasePageable.java
@@ -0,0 +1,48 @@
+package me.kvdpxne.covilo.domain.model.pagination;
+
+/**
+ * Represents a basic implementation of pagination information for querying a
+ * specific page of data.
+ */
+public class BasePageable
+  implements Pageable {
+
+  /**
+   * The index (0-based) of the requested page.
+   */
+  private final int index;
+
+  /**
+   * The size of the page.
+   */
+  private final int size;
+
+  /**
+   * Constructs a new BasePageable with the provided index and size.
+   *
+   * @param index The index (0-based) of the requested page.
+   * @param size The size of the page.
+   */
+  public BasePageable(
+    final int index,
+    final int size
+  ) {
+    this.index = index;
+    this.size = size;
+  }
+
+  @Override
+  public int getIndex() {
+    return this.index;
+  }
+
+  @Override
+  public int getSize() {
+    return this.size;
+  }
+
+  @Override
+  public long getOffset() {
+    return (long) this.index * (long) this.size;
+  }
+}
---
+++ covilo-core/src/main/java/me/kvdpxne/covilo/domain/model/pagination/Pageable.java
@@ -0,0 +1,25 @@
+package me.kvdpxne.covilo.domain.model.pagination;
+
+/**
+ * Represents pagination information for querying a specific page of data.
+ */
+public interface Pageable {
+
+  /**
+   * Retrieves the index (0-based) of the requested page.
+   * @return The index of the requested page.
+   */
+  int getIndex();
+
+  /**
+   * Retrieves the size of the page.
+   * @return The size of the page.
+   */
+  int getSize();
+
+  /**
+   * Retrieves the offset of the requested page.
+   * @return The offset of the requested page.
+   */
+  long getOffset();
+}
