008657358a6c5fe900c5045fb15ce89cefbaceb4
kvdpxne <kvdpxne@gmail.com>
2024-04-19 20:19:37 +0200

    🔌 Dropped JPA from the project.

--- covilo-core/src/main/java/me/kvdpxne/covilo/infrastructure/jpa/repositories/JpaCommentRepository.java
+++
@@ -1,8 +0,0 @@
-package me.kvdpxne.covilo.infrastructure.jpa.repositories;
-
-import me.kvdpxne.covilo.infrastructure.jpa.JpaIdentifiableRepository;
-import me.kvdpxne.covilo.infrastructure.jpa.entities.CommentEntity;
-
-public interface JpaCommentRepository
-  extends JpaIdentifiableRepository<CommentEntity> {
-}
