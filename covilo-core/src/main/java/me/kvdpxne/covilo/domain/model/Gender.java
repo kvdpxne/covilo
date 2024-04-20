7fb74d7d4d704c1fe2fd93ec44c9e8a81172d452
kvdpxne <kvdpxne@gmail.com>
2024-04-20 22:42:50 +0200

    Refactored the Gender enum to include a NONE value and added a conversion method to translate a Boolean value to the corresponding Gender enum value.

--- covilo-core/src/main/java/me/kvdpxne/covilo/domain/model/Gender.java
+++ covilo-core/src/main/java/me/kvdpxne/covilo/domain/model/Gender.java
@@ -1,8 +1,47 @@
 package me.kvdpxne.covilo.domain.model;
 
+/**
+ * Represents the gender of an individual.
+ */
 public enum Gender {
 
-  FEMALE,
+  /**
+   * Male gender.
+   */
   MALE,
-  NON_BINARY
+
+  /**
+   * Female gender.
+   */
+  FEMALE,
+
+  /**
+   * No gender specified or undefined.
+   */
+  NONE;
+
+  /**
+   * Converts a {@link Boolean} value to a corresponding {@link Gender} enum
+   * value. If the input is {@code null}, the method returns
+   * {@link Gender#NONE}, indicating no gender specified. If the input is
+   * {@code true}, it returns {@link Gender#MALE}; otherwise, it returns
+   * {@link Gender#FEMALE}.
+   *
+   * @param isMale A {@link Boolean} value indicating if the gender is male.
+   *               {@code true} represents male, {@code false} represents
+   *               female, and {@code null} represents no gender specified
+   *               ({@link Gender#NONE}).
+   * @return The {@link Gender} enum value corresponding to the {@link Boolean}
+   * input.
+   */
+  public static Gender of(
+    final Boolean isMale
+  ) {
+    // If the input is null, return NONE
+    if (null == isMale) {
+      return NONE;
+    }
+    // If the input is true, return MALE; otherwise, return FEMALE
+    return isMale ? MALE : FEMALE;
+  }
 }
